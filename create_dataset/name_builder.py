4facdd1d2f123647ba2cae0cefadb8b87d61a6f4
Benedekkiraly <bendiking@gmail.com>
2021-11-23 11:32:34 +0100

    pathfix in namebuilding

--- create_dataset/name_builder.py
+++ create_dataset/name_builder.py
@@ -1,6 +1,6 @@
 from random import randrange
 import linecache
-with open("/Users/bendo/Bendo_OneDrive/OneDrive - Kormányzati Informatikai Fejlesztési Ügynökség/Egyetem/Trento/Data_Mining/final_project/patient-therapy-recommending/create_dataset/names.txt", "w") as namesTXT:
+with open("create_dataset/names.txt", "w") as namesTXT:
     for i in range(9999):
         indexFirst =randrange(164459)
         indexLast = randrange(98413)
