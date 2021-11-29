53c3d88629716dc006e60b713909f78d8b6d2e44
Benedekkiraly <bendiking@gmail.com>
2021-11-29 21:10:54 +0100

    json builder ready, but won't work with full dataset, only reduced one

--- create_dataset/build_JSON.py
+++ create_dataset/build_JSON.py
@@ -1,6 +1,7 @@
 import json
 from random import randrange
 import datetime
+import pandas as pd
 
 i = 1
 lstConds = []
@@ -18,20 +19,22 @@ with open("/Users/bendo/Bendo_OneDrive/OneDrive - Kormányzati Informatikai Fejl
     with open("/Users/bendo/Bendo_OneDrive/OneDrive - Kormányzati Informatikai Fejlesztési Ügynökség/Egyetem/Trento/Data_Mining/final_project/patient-therapy-recommending/create_dataset/therapies.txt") as file:
         for line in file:
             lstTherapies.append(
-                {'id': j, 'type': "Therapy", 'name': line.rstrip()})
+                {'id': 'th'+str(j), 'type': "Therapy", 'name': line.rstrip()})
             j += 1
 
     myJSON = {'Conditions': lstConds, 'Therapies': lstTherapies}
 
     json.dump(myJSON, jsonFile, indent=4)
-    
+
 with open("/Users/bendo/Bendo_OneDrive/OneDrive - Kormányzati Informatikai Fejlesztési Ügynökség/Egyetem/Trento/Data_Mining/final_project/patient-therapy-recommending/create_dataset/dataset.JSON", "w") as jsonFile:
 
     with open("/Users/bendo/Bendo_OneDrive/OneDrive - Kormányzati Informatikai Fejlesztési Ügynökség/Egyetem/Trento/Data_Mining/final_project/patient-therapy-recommending/create_dataset/names.txt") as file:
         for line in file:
             i = 1
-            conditionCount = randrange(10)
+            conditionCount = randrange(1, 20)
             lstTrials = []
+            lstPreConds = []
+            finishedConds = []
             for x in range(conditionCount):
                 startDate = datetime.datetime(2015, 1, 1)
                 nextTrial = randrange(5000)
@@ -40,15 +43,37 @@ with open("/Users/bendo/Bendo_OneDrive/OneDrive - Kormányzati Informatikai Fejl
                     str(startDate)[5:7] + str(startDate)[8:10]
                 timeStampEnd = str(endDate.year) + \
                     str(endDate)[5:7] + str(endDate)[8:10]
-                conditionIndex = randrange(len(lstConds))
                 therapyIndex = randrange(len(lstTherapies))
-                success = randrange(99)
-                lstTrials.append({'id': i, 'start':  timeStampStart, 'end': timeStampEnd,
-                                 'condition': lstConds[conditionIndex]['name'], 'therapy': lstTherapies[therapyIndex]['name'], 'successful': success})
+                success = randrange(70, 101)
+                conditionID = 'pc' + str(randrange(4))
+                if conditionID not in finishedConds:
+                    trialDict = {'id': 'tr'+str(i), 'start':  timeStampStart, 'end': timeStampEnd,
+                                 'condition': conditionID, 'therapy': lstTherapies[therapyIndex]['id'], 'successful': success}
+                    lstTrials.append(trialDict)
+                    if success == 100:
+                        finishedConds.append(conditionID)
                 i += 1
+            # preconditions
+            dfx = pd.DataFrame(lstTrials)
+            try:
+                groupedConds = dfx.groupby("condition", as_index=False).agg(
+                    start=("start", "min"),
+                    end=("end", "max"),
+                    success=("successful", "max"))
+                indexMax = len(groupedConds.index)
+                for ind in range(0, indexMax):
+                    conditionIndex = randrange(len(lstConds))
+                    if groupedConds.iloc[ind]['success'] == 100:
+                        lstPreConds.append({'id': groupedConds.iloc[ind]['condition'], 'diagnosed': groupedConds.iloc[ind]
+                                            ['start'], 'cured': groupedConds.iloc[ind]['end'], 'kind': lstConds[conditionIndex]['id']})
+                    else:
+                        lstPreConds.append(
+                            {'id': groupedConds.iloc[ind]['condition'], 'diagnosed': groupedConds.iloc[ind]['start'], 'cured': "", 'kind': lstConds[conditionIndex]['id']})
+            except:
+                print("no conditions for patient")
             lstPatients.append(
-                {'id': k, 'name': line.rstrip(), 'trials': lstTrials})
+                {'id': k, 'name': line.rstrip(), 'conditions': lstPreConds, 'trials': lstTrials})
             k += 1
-
-    myJSON = {'Conditions': lstConds, 'Therapies': lstTherapies, 'Patients': lstPatients}
+    myJSON = {'Conditions': lstConds,
+              'Therapies': lstTherapies, 'Patients': lstPatients}
     json.dump(myJSON, jsonFile, indent=4)
